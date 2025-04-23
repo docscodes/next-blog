ed7574ad4b2c93f3885b3430f15e81ace82f4d4c
docscodes <docscodes@gmail.com>
2025-04-23 17:32:21 -0400

    card component

--- app/page.js
+++ app/page.js
@@ -1,3 +1,24 @@
+function Card(props) {
+  return (
+    <div className="border rounded-md border-gray-600 p-4">
+      Card component: {props.text}
+    </div>
+  );
+}
+
 export default function Home() {
-  return <div>Hello World</div>;
+  const name = "Piotr";
+
+  return (
+    <>
+      <div className="p-20 space-y-4">
+        <div>Hello, {name}</div>
+        <Card text="This is being passed from the parent" />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  );
 }
