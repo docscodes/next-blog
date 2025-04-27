60f1e37fc2f72e961c253864efc9e0598d60e9f6
docscodes <docscodes@gmail.com>
2025-04-27 08:40:03 -0400

    links and navigation

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Header from "@/components/header";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -21,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <header className="p-20 border border-yellow-400">I am be visible everywhere!</header>
+        <Header />
         <main>{children}</main>
       </body>
     </html>
