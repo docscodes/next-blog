a74e0e948d74ddc7330039cb15a38c040892765e
docscodes <docscodes@gmail.com>
2025-04-27 13:18:06 -0400

    positioning with flex

--- components/header.js
+++ components/header.js
@@ -1,11 +1,17 @@
-import Counter from "./counter";
+import Link from "next/link";
 import Navigation from "./navigation";
 
 export default function Header() {
   return (
-    <header>
-      <Navigation />
-      <Counter />
+    <header className="flex justify-between md:items-center mt-4">
+      <div className="flex items-center md:space-x-12">
+        <div className="hidden md:block">
+          <Link href="/" className="text-xl">My Name</Link>
+        </div>
+        <Navigation />
+      </div>
+
+      <div>Dark Toggle</div>
     </header>
   );
 }
