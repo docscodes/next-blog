d92e27700b93e2d13513ae7d2308cd1d54bea524
docscodes <docscodes@gmail.com>
2025-04-28 15:00:41 -0400

    loading mdx from files

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,5 +1,8 @@
+import fs from "fs";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { notFound } from "next/navigation";
+import path from "path";
+
 const titles = {
   first: "Hello First!",
   second: "Hello Second!",
@@ -15,17 +18,31 @@ export async function generateMetadata({ params }, parent) {
 }
 
 export default function BlogPage({ params }) {
-  if (!["first", "second"].includes(params.slug)) {
+  // if (!["first", "second"].includes(params.slug)) {
+  //   notFound();
+  // }
+
+  let markdown;
+
+  try {
+    markdown = fs.readFileSync(path.join(process.cwd(), "content", `${params.slug}.mdx`));
+  } catch (e) {
     notFound();
   }
 
   return (
     <article className="prose dark:prose-invert">
-      <MDXRemote source={`# Hello World
-
-      This is from Server Components!
-      `}
-      />
+      <MDXRemote source={markdown} />
     </article>
   );
+
+  // return (
+  //   <article className="prose dark:prose-invert">
+  //     <MDXRemote source={`# Hello World
+
+  //     This is from Server Components!
+  //     `}
+  //     />
+  //   </article>
+  // );
 }
