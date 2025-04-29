922c3f10b1fb441c4190f513a519453f7a90578e
docscodes <docscodes@gmail.com>
2025-04-28 22:16:56 -0400

    extracting frontmatter and content

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,47 +1,21 @@
-import { loadPost } from "@/lib/posts";
-import { MDXRemote } from "next-mdx-remote/rsc";
+import { getPost } from "@/lib/posts";
 import { notFound } from "next/navigation";
 
-const titles = {
-  first: "Hello First!",
-  second: "Hello Second!",
-};
-
 export async function generateMetadata({ params }, parent) {
-  const description = (await parent).description ?? "Default description";
-
-  return {
-    title: titles[params.slug],
-    description,
-  };
+  try {
+    const { frontmatter } = await getPost(params.slug);
+    return frontmatter;
+  } catch (e) {}
 }
 
-export default function BlogPage({ params }) {
-  // if (!["first", "second"].includes(params.slug)) {
-  //   notFound();
-  // }
-
-  let markdown;
+export default async function BlogPage({ params }) {
+  let post;
 
   try {
-    markdown = loadPost(params.slug);
+    post = await getPost(params.slug);
   } catch (e) {
     notFound();
   }
 
-  return (
-    <article className="prose dark:prose-invert">
-      <MDXRemote source={markdown} />
-    </article>
-  );
-
-  // return (
-  //   <article className="prose dark:prose-invert">
-  //     <MDXRemote source={`# Hello World
-
-  //     This is from Server Components!
-  //     `}
-  //     />
-  //   </article>
-  // );
+  return <article className="prose dark:prose-invert">{post.content}</article>;
 }
