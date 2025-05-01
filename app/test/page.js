dea1f4e56fd55b10fb556bd5a77603350a182343
docscodes <docscodes@gmail.com>
2025-04-30 20:36:50 -0400

    update home page

---
+++ app/test/page.js
@@ -0,0 +1,36 @@
+"use client";
+
+import Card from "@/components/card";
+import { useState } from "react";
+
+export default function Home({ searchParams }) {
+  const [isVisible, setIsVisible] = useState(true);
+  const [names, setNames] = useState(["Piotr", "John", "Terry"]);
+  const name = "Alice";
+
+  const handleClick = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const handleAdd = () => {
+    setNames([...names, "New element!"]);
+  };
+
+  const cards = isVisible && names.map((name, index) => <Card key={index}>{name}</Card>);
+
+  console.log(searchParams);
+  if (searchParams.error) throw new Error("Hello!");
+
+  return (
+    <>
+      <div className="space-y-4">
+        <div>Hello, {name}</div>
+        {cards}
+        <div className="flex space-x-4">
+          <button onClick={handleClick}>{isVisible ? "Hide" : "Show"}</button>
+          <button onClick={handleAdd}>Add</button>
+        </div>
+      </div>
+    </>
+  );
+}
--- components/header.js
+++ components/header.js
@@ -11,7 +11,7 @@ export default function Header() {
     <header className="flex justify-between md:items-center mt-4">
       <div className="flex items-center md:space-x-12">
         <div className="hidden md:block">
-          <Link href="/" className="text-xl">My Name</Link>
+          <Link href="/" className="text-xl font-mono">My Name</Link>
         </div>
         <Navigation />
       </div>
